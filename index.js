979078dcf05e6497e51e102daf3b90eabc91233e
Kacperek <61110867+kacpereqo@users.noreply.github.com>
2020-04-28 15:07:37 +0200

    destroyer

---
+++ index.js
@@ -0,0 +1,17 @@
+function onload()
+{
+ alert2();
+ alert1();
+ setTimeout('alert()',500);
+ setTimeout('alert2()',1000);
+}
+function alert1() 
+{
+    window.open('http://kacpereqo.github.io', '_blank');
+    
+}
+function alert2()
+{
+    alert('nic nie wygrałeś !')
+    
+}
