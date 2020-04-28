e9334eca182e46527951c9e2e1259bd71ec3a7b8
Kacperek <61110867+kacpereqo@users.noreply.github.com>
2020-04-28 16:00:28 +0200

    .

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 function onload()
 {
  alert2();
- alert1();
- setTimeout('alert()',30000);
+
  setTimeout('alert2()',10000);
-}
-function alert1() 
-{
-    window.open('http://kacpereqo.github.io', '_blank');
-    
 }
 function alert2()
 {
