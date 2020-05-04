b14d791fb6471d32d412bb79b01d1c92afe7cb1b
Kacperek <61110867+kacpereqo@users.noreply.github.com>
2020-05-04 08:38:11 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+function odliczanie()
+	{
+		var today = new Date();
+		
+		var day = today.getDate();
 
-function alert()
-{
-    alert('nic nie wygrałeś !')
-    setTimeout('alert2()',10000);
-    
-}
+		var month = today.getMonth()+1;
+		if (month<10) month = "0"+month;
+
+		var year = today.getFullYear();
+		
+		var hour = today.getHours();
+		if (hour<10) hour = "0"+hour;
+		
+		var minute = today.getMinutes();
+		if (minute<10) minute = "0"+minute;
+		
+		var second = today.getSeconds();
+		if (second<10) second = "0"+second;
+
+		
+
+		document.getElementById("timer").innerHTML = day+"."+month+"."+year+" | "+hour+":"+minute+":"+second; 
+		 setTimeout("odliczanie()",1000);
+	}
