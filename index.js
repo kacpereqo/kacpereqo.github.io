b2a45c281e3ba2214c6c9b9796b9bb12263b3fb8
Kacperek <61110867+kacpereqo@users.noreply.github.com>
2020-06-22 13:17:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 let complete = 1;
 let font=0.2;
+var xd=0;
 function loading()
 {
     const bar = document.getElementById('loading');
     complete+=0.1;
     if(complete>=99)
     {
+        xd=1;
         complete=99;
     }
     font+=0.03;
     if (font>=20)
     {
      font=20;
+    }
+    if (xd==1)
+    {
+        complete-=0.2;
     }
     console.log(complete);
     bar.innerHTML= Math.round(complete)+'% ukonczone';
